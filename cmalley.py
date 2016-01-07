df95a694b0a9d6071cc00479ecbc5180009927a2
C <Connor@Computer.local>
2016-01-07 15:05:42 -0700

    Hoff

---
+++ cmalley.py
@@ -0,0 +1,10 @@
+import subprocess
+def do_something():
+    promt = raw_input("The situation is unchangable, yet you cannot push the river. Y/N ")
+    if promt == "Y":
+        print "With age comes wisdom, have a nice day. "
+
+    else:
+        print: "gggggggggg"
+        subprocess.call(['osascript', '-e',
+        'tell app "System Events" to shut down'])
